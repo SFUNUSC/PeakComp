8f31b57fbc47744c8ad58d50970ac532838f5d63
Jonathan Williams <ejw1@sfu.ca>
2016-05-06 11:25:44 -0700

    Fixed saving of scaled data and background to disk (previous commits had broken this).  Switched linear equation solver to new code from the Gridlock program.

--- utils/lin_eq_solver.h
+++ utils/lin_eq_solver.h
@@ -4,19 +4,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 
 #define MAX_DIM 20
 
 typedef struct
 {
+  //properties set by the user
   long double matrix[MAX_DIM][MAX_DIM];
   long double vector[MAX_DIM];
+  int dim;
+  //properties determined by the solver
+  long double inv_matrix[MAX_DIM][MAX_DIM];//inverse of matrix specified above
   long double solution[MAX_DIM];
-  int    dim;
 }lin_eq_type;
 
 int solve_lin_eq(lin_eq_type *lin_eq);
-long double  det(int m, lin_eq_type *lin_eq);
+long double det(int m, lin_eq_type *lin_eq);
+int get_inv(lin_eq_type *lin_eq);
 
 #endif
